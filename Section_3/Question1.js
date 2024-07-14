38fc3998848471eea87be137b27aed04120913b2
Sami Ullah <157210731+SamiiiU@users.noreply.github.com>
2024-07-14 22:41:00 +0500

    Create Question1.js

---
+++ Section_3/Question1.js
@@ -0,0 +1,6 @@
+// 1. Map Transformation:
+// Q: Given an array of integers, use the `map` method to square each element and return a new array with the squared values.
+
+const numbers = [1, 2, 3, 4, 5];
+const squaredNumbers = numbers.map(num => num * num);
+console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
